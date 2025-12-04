5a810dbb4b22bf76d8d03b03c1fb8130478dfc28
0001159198@senaimgaluno.com.br <0001159198@senaimgaluno.com.br>
2025-12-04 10:03:41 -0300

    novo commit

--- escolaatualizada.sql
+++ escolaatualizada.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/11/2025 às 15:17
+-- Tempo de geração: 04/12/2025 às 13:50
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `alunos`
---
-
-CREATE TABLE `alunos` (
-  `id` int(10) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `alunos`
---
-
-INSERT INTO `alunos` (`id`, `nome`, `email`) VALUES
-(1, 'd', 'd');
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `professores`
 --
 
@@ -56,41 +37,32 @@ CREATE TABLE `professores` (
   `materia_responsavel` varchar(100) NOT NULL,
   `data_criacao` datetime NOT NULL DEFAULT current_timestamp(),
   `data_atualizacao` datetime NOT NULL DEFAULT current_timestamp(),
-  `data_remocao` datetime DEFAULT NULL
+  `data_remocao` datetime DEFAULT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `professores`
 --
 
-INSERT INTO `professores` (`id`, `nome`, `email`, `estado`, `cidade`, `estado_civil`, `materia_responsavel`, `data_criacao`, `data_atualizacao`, `data_remocao`) VALUES
-(1, 'Davi', 'd@gmail.com', 'mg', 'Belo Horizonte', 'soteiro', 'ciencia', '2025-11-27 10:10:20', '2025-11-27 10:10:20', NULL);
+INSERT INTO `professores` (`id`, `nome`, `email`, `estado`, `cidade`, `estado_civil`, `materia_responsavel`, `data_criacao`, `data_atualizacao`, `data_remocao`, `usuario`, `senha`) VALUES
+(1, 'Davi', 'd@gmail.com', 'mg', 'Belo Horizonte', 'soteiro', 'ciencia', '2025-11-27 10:10:20', '2025-11-27 10:10:20', NULL, '', '');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `alunos`
---
-ALTER TABLE `alunos`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Índices de tabela `professores`
 --
 ALTER TABLE `professores`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
-
---
--- AUTO_INCREMENT de tabela `alunos`
---
-ALTER TABLE `alunos`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `professores`
